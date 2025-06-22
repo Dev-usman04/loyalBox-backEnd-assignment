54e9561a00793406c890a6d18c3503e2cf0a9b37
DevDre <bighalo77@gmail.com>
2025-06-22 17:49:03 +0100

    update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const fs = require('fs');
-const swaggerUi = require('swagger-ui-express');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const fs = require("fs");
+const swaggerUi = require("swagger-ui-express");
 
 dotenv.config();
 const app = express();
@@ -13,15 +13,18 @@ const app = express();
 app.use(express.json());
 
 // Serve Swagger UI
-const swaggerDocument = JSON.parse(fs.readFileSync('./swagger.json', 'utf8'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocument = JSON.parse(fs.readFileSync("./swagger.json", "utf8"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Connect to MongoDB
 connectDB();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.get("/", (req, res) => {
+  res.send(`Welcome to the LoyalBox API`);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
